dd454d7979ff2f51d6ae164a4ebfe56f7648f6dd
Meredith Jones <mj19003@gmail.com>
2021-01-12 17:55:42 -0500

    Added functionality to server.js

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,13 +2,13 @@
 -- with at least three entries.
 
 
-INSERT INTO burgers (name) VALUES ('');
-INSERT INTO burgers (name) VALUES ('');
-INSERT INTO burgers (name) VALUES ('');
-INSERT INTO burgers (name) VALUES ('');
-INSERT INTO burgers (name) VALUES ('');
-INSERT INTO burgers (name) VALUES ('');
--- INSERT INTO burgers (name, ) VALUES ('', true);
--- INSERT INTO burgers (name, ) VALUES ('', true);
--- INSERT INTO burgers (name, ) VALUES ('', true);
--- INSERT INTO burgers (name) VALUES ('');
+INSERT INTO burgers (burger_name)
+VALUE ('Black_bean_burger');
+INSERT INTO burgers (burger_name)
+VALUE ('Impossible_burger');
+INSERT INTO burgers (burger_name)
+VALUE ('Beyond_Burger');
+INSERT INTO burgers (burger_name)
+VALUE ('House_Made_Veggie_Burger');
+INSERT INTO burgers (burger_name)
+VALUE ('Falafal_burger');
--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 //Require Express
-const express = require('express')
-const app = express()
+var express = require("express")
+var app = express()
 
 // Set the port of our application
 // process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Require Handlebars.
+var exphbs = require("express-handlebars");
+
+//Set the view engine to handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_controller.js");
+
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
