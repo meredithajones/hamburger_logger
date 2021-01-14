a1cb8df3466a7700de7a2f68465546ba0bb883f1
Meredith Jones <mj19003@gmail.com>
2021-01-13 22:31:03 -0500

    Update with JawsDB connection code added

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,21 @@
 
 var mysql = require("mysql");
 
+var connection;
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'nameOfYour_db'
+    })
+};
+
 var PORT = process.env.PORT || 3000;
 
 var connection = mysql.createConnection({
